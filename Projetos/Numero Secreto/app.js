c7bf4b657f327ef0b1942ae7a69c0c8d7aa52d30
Diego Bernardes Silva <diego.b.silva@simec.local>
2025-10-02 16:37:32 -0300

    feat: importando bibliotecas, e configurando ambiente de desenvolvimento

--- Projetos/Numero Secreto/app.js
+++ Projetos/Numero Secreto/app.js
@@ -2,11 +2,16 @@ alert("Bem vindo ao jogo do número secreto.")
 
 let numeroSecreto = 10;
 console.log(numeroSecreto);
+
 let chute = prompt("Digite um número:");
+console.log("Valor do chute:", chute);
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    alert("Isso aí! Você descobriu o número secreto" + " :" + numeroSecreto + ".");
+    console.log("Resultado da comparação:", chute == numeroSecreto);
+    alert(`Isso aí! Você descobriu o número secreto! ${numeroSecreto}`);
 } else {
     alert("Você errou! :(");
+    console.log("Valor do número secreto:", numeroSecreto);
+    alert(`O número secreto era ${numeroSecreto}`)
 }
