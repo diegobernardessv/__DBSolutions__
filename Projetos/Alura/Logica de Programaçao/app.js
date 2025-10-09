ae271c2241f67f7c579b507fa8563f5dcabc9198
Diego Bernardes Silva <diego.b.silva@simec.local>
2025-10-09 17:07:05 -0300

    update projeto numero secreto

--- Projetos/Alura/Logica de Programaçao/app.js
+++ Projetos/Alura/Logica de Programaçao/app.js
@@ -1,6 +1,1 @@
-let contador = 1;
 
-while (contador < 4) {
-    console.log('Executando a interação' + contador);
-    contador++;
-}
--- Projetos/Numero Secreto/app.js
+++ Projetos/Numero Secreto/app.js
@@ -1,16 +1,16 @@
 alert("Bem vindo ao jogo do número secreto.")
 
-let numeroSecreto = 6;
+let numeroSecreto = parseInt(Math.random(1, 101) * 100);
 let palpite; 
 let tentativas = 1;
+console.log(`O número secreto é: '${numeroSecreto}`);
 
 // enquanto palpite não for igual ao n° secreto
 while(palpite != numeroSecreto) {
-    palpite = prompt("Escolha um número de 1 a 10");
+    palpite = prompt("Escolha um número de 1 a 100");
 // se palpite for igual ao número secreto
     if (palpite == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto! O número secreto é o ${numeroSecreto}!`);
-        alert(`Você acertou em ${tentativas} tentativas.`)
+        break;
     } else {
         if (palpite > numeroSecreto) {
             alert(`O número secreto é menor do que o ${palpite}.`);
@@ -21,3 +21,8 @@ while(palpite != numeroSecreto) {
             tentativas++;
     }
  }
+
+ // Diferenciando tentativa maior e menor do que 1;
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+    alert(`Isso aí! Você descobriu o número secreto! O número secreto é o ${numeroSecreto}!`);
+    alert(`Você acertou em ${tentativas} ${palavraTentativa}.`)
