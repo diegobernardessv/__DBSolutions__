ccd3de5329a08de08ce5a42b43756e5b2173c2a4
Diego Bernardes Silva <diegobernardessv@gmail.com>
2025-10-16 00:05:16 -0300

    feat: project 100 days of code - Python

--- Projetos/100 Days of Code - The Complete Python Pro Bootcamp/Day 3/Nesting and Elif/task.py
+++ Projetos/100 Days of Code - The Complete Python Pro Bootcamp/Day 3/Nesting and Elif/task.py
@@ -1,7 +1,25 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+print("=== Bem vindo á Montanha Russa Sombria! ===\n")
+altura = int(input("Qual é a sua altura em centímetros? "))
 
-if height >= 120:
-    print("You can ride the rollercoaster")
+if altura >= 120:
+    print("Você pode andar na Montanha Russa!\n")
+
+    idade_validada = False
+    # Validação da idade
+    while not idade_validada:
+        idade = int(input("Qual é a sua idade? "))
+
+        if idade < 1 or idade > 100:
+            print("Opção inválida. Por favor, insira uma idade entre 1 e 100.")
+        # Lógica de preços para idades válidas
+        elif idade <= 12:
+            print("Você deve pagar $5.")
+            idade_validada = True
+        elif idade <= 18:
+            print("Você deve pagar $7.")
+            idade_validada = True
+        else:  # Idade maior que 18 e válida
+            print("Você deve pagar $12.")
+            idade_validada = True
 else:
-    print("Sorry you have to grow taller before you can ride.")
+    print("Desculpe. Você precisa ficar mais alto para andar na montanha russa!")
