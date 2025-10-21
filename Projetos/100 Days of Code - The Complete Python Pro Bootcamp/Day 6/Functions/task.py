cb57d2e28d032fc98a2068a84722c2987a82bdbf
Diego Bernardes Silva <diego.b.silva@simec.local>
2025-10-21 08:41:52 -0300

    projeto: 100 days of code

---
+++ Projetos/100 Days of Code - The Complete Python Pro Bootcamp/Day 6/Functions/task.py
@@ -0,0 +1,5 @@
+def soma_numeros(num1, num2):
+    soma_num = num1 + num2
+    print(soma_num)
+
+soma_numeros(10, 100)
---
+++ Projetos/Interface Tkinter/interface_grafica.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+janela = tk.Tk()
+janela.title("Calculadora")
+
+
+janela.mainloop()
+
+
